4e1382c45463c5e66565f85377722b3f92a9702f
Nikitadhokane <100338235+Nikitadhokane@users.noreply.github.com>
2023-10-19 11:30:16 +0530

    Add files via upload
     In this I was created a simple Spring Boot Project. It's a simple web Application. It includes ' TestRun' that handles request mapping ("/test"). The 'firstOutput()' method returns the string "Hello I am Running" , The SpringbootdemoprojectApplication' is the main class of the application annotated with @SpringBootApplication which indicates that it is a Springboot application when executed. The line 'server.port=80880' indicates that the application is run on the port 8080. It means that when I run the application, It will be accessible through the URL 'http://localhost:8080'.

---
+++ springbootdemoproject1/src/main/java/com/example/demo/Springbootdemoproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootdemoproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootdemoproject1Application.class, args);
+	}
+
+}
---
+++ springbootdemoproject1/src/main/java/com/example/demo/TestRun.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@ResponseBody
+public class TestRun {
+	
+	@RequestMapping("/test")
+	public String firstOutput()
+	{
+		return "Hello I am running";
+	}
+
+
+}

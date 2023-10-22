2849ed1b1c6494dfd81e6df5c66eaef62ff0964b
Nikitadhokane <100338235+Nikitadhokane@users.noreply.github.com>
2023-10-22 22:43:42 +0530

    Delete springbootfoodsystemproject directory

--- springbootfoodsystemproject/src/main/java/com/sb/foodsystem/dao/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sb.foodsystem.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sb.foodsystem.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin,Long>{
-
-}
--- springbootfoodsystemproject/src/main/java/com/sb/foodsystem/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sb.foodsystem.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sb.foodsystem.entity.User;
-
-public interface UserRepository  extends JpaRepository<User,String> {
-
-}
